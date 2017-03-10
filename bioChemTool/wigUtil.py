3f4e1d42fa1a60070b1fead096bef8a9cd1cd98e
Staph. aureus <svz5160@psu.edu>
2017-03-09 23:48:08 -0500

    Update wigutil

--- bioChemTool/wigUtil.py
+++ bioChemTool/wigUtil.py
@@ -1,14 +1,25 @@
+class equalDict(dict):
+    def __str__(self):
+        result = ''
+        for item in self.keys():
+            if self[item] is not None:
+                result += item+'='+self[item]+' '
+        return result[:-1]
+
 class ucscFile():
-    def __init__(self,name,description='',visibility='hide',color=[255,255,255],priority=100,additionConf='',browserConf=None):
+    '''Universal file structure for UCSC Genome Sequence files including wig
+        and '''
+    def __init__(self,name,description='',visibility='hide',color='255,255,255',priority='100',additionConf='',browserConf=None):
+        self.config = equalDict()
         self.type = 'unknown'
-        self.name = name
-        self.desc = description
-        self.visi = visibility
-        self.color = color
-        self.prio = priority
+        self.config['name'] = name
+        self.config['description'] = description
+        self.config['visibility'] = visibility
+        self.config['color'] = color
+        self.config['priority'] = priority
         self.addn = additionConf
         if browserConf is None:
-            self.brow = browserConfig()
+            self.brow = equalDict()
         else:
             self.brow = browserConf
         self.data = []
@@ -54,9 +65,12 @@ Has two types:
     variableStep - varStep = True (default)
     fixedStep - varStep = False
 Need to specify chromosome when initializing.'''
-    def __init__(self,chromosome,span,varStep=True):
+    def __init__(self,chromosome,span,varStep=True,start=None):
         self.chr = chromosome
         self.type = varStep
+        self.start = start
+        if not varStep and not start:
+            raise SyntaxError('fixedStep requires start position.')
         self.span = span
         self.data = []
     def __str__(self):
@@ -65,9 +79,25 @@ Need to specify chromosome when initializing.'''
         else:
             result = 'fixedStep '
         result += 'chrom='+self.chr
-        result += ' span='+str(self.span)
+        if varStep:
+            if span:
+                result += ' span='+str(self.span)
+            result += '\n'
+            for item in self.data:
+                result += str(item[0])+' '+str(item[1])+'\n'
+        else:
+            result += ' start='+str(self.start)
+            result += ' step='+str(self.span)
         return result
+    def __getitem__(self,key):
+        return self.data[key]
+    def __setitem__(self,key,item):
+        self.data[key] = item
+    def __iter__(self):
+        return self.data.__iter__()
+    def append(self,item):
+        self.data.append(item)
+    def pop(self):
+        return self.data.pop()
 
-class browserConfig():
-    def __init__(self):
-        self.data = {}
+
